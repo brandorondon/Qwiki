e0ab158179da9c5572cff9f0f24cf2849d2b2020
aidangainor <againor@brandeis.edu>
2016-11-28 22:14:28 -0500

    Spring MVC hello world running locally and on cluster w/ Tomcat

---
+++ query/src/main/java/qwiki/gui/WebInterfaceConfig.java
@@ -0,0 +1,24 @@
+package qwiki.gui;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "qwiki.gui")
+public class WebInterfaceConfig {
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+ 
+        return viewResolver;
+    }
+}
